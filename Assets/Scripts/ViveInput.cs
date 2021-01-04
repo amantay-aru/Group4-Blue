b23ffca928663ea4f17eb13faf67731158a9790f
Moritz Wölk <woelk.moritz@gmail.com>
2021-01-04 20:14:23 +0100

    Bus stop and bus added

---
+++ Assets/Scripts/ViveInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ()
+        {
+
+        }
+    }
+}
